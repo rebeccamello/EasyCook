f28dc416fcd0c5c790615f93fe8961870d13eb10
Rebecca Mello <49920539+rebeccamello@users.noreply.github.com>
2021-05-05 20:00:08 -0300

    Adicionando código das classes
    Classes de Chefe, Receitas, Admin e Comentários

---
+++ easycook_classes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Chefe{
+    string nome;
+    string username;
+    string senha;
+    string email;
+    string foto;
+    Receita favoritadas[100];
+    Receita postadas[100];
+    bool verificado;
+};
+
+class Receita{
+    string titulo;
+    float avaliacao;
+    string tipoCulinaria;
+    string equipamentos[100];
+    int numComentarios;
+    Comentario comentarios[100];
+    string preparo;
+    string restricoes[100];
+    string ingredientes[100];
+    float tempoPreparo;  
+};
+
+class Admin{
+    string email;
+    string senha;
+};
+
+class Comentario{
+    string comentario;
+    string timestamp;
+};
